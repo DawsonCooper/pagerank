3cdc3c57361b51758fc519d2ef985d0082fc63df
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-11-16 08:10:36 -0600

    Added transition model and outline for sample functino

--- pagerank.py
+++ pagerank.py
@@ -50,14 +50,31 @@ def crawl(directory):
 
 def transition_model(corpus, page, damping_factor):
     """
-    Return a probability distribution over which page to visit next,
-    given a current page.
+    Return a dictionary probability distribution over which page to visit next,
+    given a current page. 
+    ex. {pageKey/link: probability, pagekey/link: probability, ...}
+    
 
     With probability `damping_factor`, choose a link at random
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    # total distribution of any page being chosen key = page value = probability
+    distribution = dict()
+    # set of pages that are linked by the current page 
+    currentPageLinks = corpus[page]
+    
+    # calculate the base probability for each page in the corpus being chosen completly at random after the damping factor is applied and divided evenly among all pages in the corpus
+    for page in corpus:
+        distribution[page] = (1 - damping_factor) / len(corpus)
+        
+    # for each page that is linked by current add the probability of being chosen at random to the distribution of all pages being chosen
+    for page in currentPageLinks:
+        evenDistribution = damping_factor / len(currentPageLinks)
+        distribution[page] += evenDistribution
+    
+    print(distribution)
+    return distribution
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -68,8 +85,25 @@ def sample_pagerank(corpus, damping_factor, n):
     Return a dictionary where keys are page names, and values are
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
+    
+    result = {pageInCorpus: probability, pageInCorpus: probability, ...}
+    
     """
-    raise NotImplementedError
+    result = dict()
+    # this page will start as a random page then will be updated throughout our sample loop 
+    currPage = random.choice(list(corpus.keys()))
+    """
+    sample loop starts on a random page then chooses a page based on the probability distribution of returned from transition_model
+    each time we visit a page we can increment the value of that page in our result dictionary by 1
+    after the loop is finished we can loop over our result dictionary and divide each value by the total number of samples to get the probability of each page being chosen
+    """
+    for i in range(n):
+        
+        pass
+    
+    
+    
+    return result
 
 
 def iterate_pagerank(corpus, damping_factor):
